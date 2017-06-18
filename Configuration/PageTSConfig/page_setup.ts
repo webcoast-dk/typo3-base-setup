183cb6ee9e17db204db2407f77f2e0ebec22df18
Thorben Nissen <thorben.nissen@kapp-hamburg.de>
2017-06-18 15:17:33 +0200

    [TASK] Set default permissions

--- Configuration/PageTSConfig/page_setup.ts
+++ Configuration/PageTSConfig/page_setup.ts
@@ -1,1 +1,8 @@
+TCEMAIN {
+    permissions {
+        groupid = 1
+        group = show,edit,delete,new,editcontent
+    }
+}
+
 <INCLUDE_TYPOSCRIPT: source="DIR:EXT:typo3_default_setup/Configuration/PageTSConfig/Includes" extensions="ts">
