76244ff633c26ce1017b5c49f91b6ae8a143a398
Thorben Nissen <thorben.nissen@kapp-hamburg.de>
2017-11-15 18:45:10 +0100

    [BUGFIX] Move registration of ts config to pages TCA

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,3 +1,5 @@
 <?php
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 ExtensionManagementUtility::registerPageTSConfigFile('typo3_base_setup', 'Configuration/PageTSConfig/page_setup.ts', 'Page setup');
