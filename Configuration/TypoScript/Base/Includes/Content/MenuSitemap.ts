0bb08561bde6e0970087f8323d6d9c1e973f3375
Thorben Nissen <thorben.nissen@kapp-hamburg.de>
2017-12-01 16:23:17 +0100

    [FEATURE] Add better default to `menu_sitemap`
    * reconfigure data processing to use the homepage as start
    * add custom template

---
+++ Configuration/TypoScript/Base/Includes/Content/MenuSitemap.ts
@@ -0,0 +1,9 @@
+tt_content.menu_sitemap {
+    dataProcessing {
+        10 = WEBcoast\Typo3BaseSetup\DataProcessing\MenuProcessor
+        10 {
+            special >
+            entryLevel = 0
+        }
+    }
+}
