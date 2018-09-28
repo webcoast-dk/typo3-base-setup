6c60e0648f26b580ee1dbb491a6901d9c0e8a82b
Thorben Nissen <tni@combine.dk>
2018-09-28 14:04:26 +0200

    [TASK] Dependencies: Allow TYPO3 CMS 9

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,8 +20,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.0.0-dev',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '8.7.0-8.99.99',
-            'rte_ckeditor' => '8.7.0-8.99.99',
+			'typo3' => '8.7.0-9.99.99',
+            'rte_ckeditor' => '8.7.0-9.99.99',
 		),
 		'conflicts' => array(
 		),
