0ced5f519fbcf5f5f5651f8fad4efb6561de0077
Thorben Nissen <thorben.nissen@kapp-hamburg.de>
2018-12-23 17:22:11 +0100

    [CLEANUP] Move `addStaticFile` to TCA overrides

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,40 @@
+<?php
+
+// add default setup
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+ExtensionManagementUtility::addStaticFile(
+    'typo3_base_setup',
+    'Configuration/TypoScript/Base',
+    'Basic page setup (main)'
+);
+// add language menu setup
+ExtensionManagementUtility::addStaticFile(
+    'typo3_base_setup',
+    'Configuration/TypoScript/LanguageMenu',
+    'Basic page setup (language menu)'
+);
+// add breadcrumb menu setup
+ExtensionManagementUtility::addStaticFile(
+    'typo3_base_setup',
+    'Configuration/TypoScript/BreadcrumbMenu',
+    'Basic page setup (breadcrumb menu)'
+);
+// add header menu setup
+ExtensionManagementUtility::addStaticFile(
+    'typo3_base_setup',
+    'Configuration/TypoScript/HeaderMenu',
+    'Basic page setup (header menu)'
+);
+// add footer menu setup
+ExtensionManagementUtility::addStaticFile(
+    'typo3_base_setup',
+    'Configuration/TypoScript/FooterMenu',
+    'Basic page setup (footer menu)'
+);
+// add sub menu setup
+ExtensionManagementUtility::addStaticFile(
+    'typo3_base_setup',
+    'Configuration/TypoScript/SubMenu',
+    'Basic page setup (sub menu)'
+);
--- ext_tables.php
+++ ext_tables.php
@@ -5,43 +5,6 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied!');
 }
 
-// add default setup
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
-    'Configuration/TypoScript/Base',
-    'Basic page setup (main)'
-);
-// add language menu setup
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
-    'Configuration/TypoScript/LanguageMenu',
-    'Basic page setup (language menu)'
-);
-// add breadcrumb menu setup
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
-    'Configuration/TypoScript/BreadcrumbMenu',
-    'Basic page setup (breadcrumb menu)'
-);
-// add header menu setup
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
-    'Configuration/TypoScript/HeaderMenu',
-    'Basic page setup (header menu)'
-);
-// add footer menu setup
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
-    'Configuration/TypoScript/FooterMenu',
-    'Basic page setup (footer menu)'
-);
-// add sub menu setup
-TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
-    'Configuration/TypoScript/SubMenu',
-    'Basic page setup (sub menu)'
-);
-
 // register backend layout provider
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['BackendLayoutDataProvider']['typo3_base_setup'] = WEBcoast\Typo3BaseSetup\Backend\View\FileBackendLayoutProvider::class;
 $extensionConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
