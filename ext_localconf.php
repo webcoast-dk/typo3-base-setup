75a15056094c3b138566cc4b23af3cb6a358be52
Thorben Nissen <thorben@webcoast.dk>
2020-09-04 16:39:47 +0200

    [FEATURE] Picture tag renderer
    * add picture tag media renderer to be used with `f:media` view helper for images

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,3 +9,10 @@ if ((bool)$extensionConfiguration['enableEmbedYouTubeHelper'] === true) {
     // register embed youtube helper
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['onlineMediaHelpers']['youtube'] = WEBcoast\Typo3BaseSetup\Resources\OnlineMedia\Helpers\EmbedYouTubeHelper::class;
 }
+
+/** @var \TYPO3\CMS\Core\Configuration\ExtensionConfiguration $extensionConfiguration */
+$extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class);
+if ($extensionConfiguration->get('typo3_base_setup', 'enablePictureTagRenderer')) {
+    $rendererRegistry = \TYPO3\CMS\Core\Resource\Rendering\RendererRegistry::getInstance();
+    $rendererRegistry->registerRendererClass(\WEBcoast\Typo3BaseSetup\Resources\Rendering\PictureTagRenderer::class);
+}
