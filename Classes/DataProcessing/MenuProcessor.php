5eaa57000bd76f27f37c98f6be9c847af6c94418
Thorben Nissen <thorben@webcoast.dk>
2021-05-16 16:39:04 +0200

    [CLEANUP] MenuProcessor: Remove old link logic
    * remove logic for determine the final link parameter for pages of type shortcut and external url

--- Classes/DataProcessing/MenuProcessor.php
+++ Classes/DataProcessing/MenuProcessor.php
@@ -25,29 +25,6 @@ class MenuProcessor extends \TYPO3\CMS\Frontend\DataProcessing\MenuProcessor
         $this->allowedConfigurationKeys[] = 'addQueryParams';
     }
 
-    /**
-     * Restore the correct shortcut value if it was overwritten by an empty overlay
-     *
-     * @param $page
-     */
-    protected function determineOriginalShortcutPage(&$page)
-    {
-        // Check if modification is required
-        if (
-            $this->getTypoScriptFrontendController()->sys_language_uid > 0
-            && empty($page['shortcut'])
-            && !empty($page['uid'])
-            && !empty($page['_PAGES_OVERLAY'])
-            && !empty($page['_PAGES_OVERLAY_UID'])
-        ) {
-            // Using raw record since the record was overlaid and is correct already:
-            $originalPage = $this->pageRepository->getRawRecord('pages', $page['uid']);
-
-            if ($originalPage['shortcut_mode'] === $page['shortcut_mode'] && !empty($originalPage['shortcut'])) {
-                $page['shortcut'] = $originalPage['shortcut'];
-            }
-        }
-    }
 
     /**
      * @return TypoScriptFrontendController
@@ -55,46 +32,6 @@ class MenuProcessor extends \TYPO3\CMS\Frontend\DataProcessing\MenuProcessor
     protected function getTypoScriptFrontendController()
     {
         return $GLOBALS['TSFE'];
-    }
-
-    public function buildConfiguration()
-    {
-        $this->menuLevelConfig['stdWrap.']['cObject.']['100'] = 'USER';
-        $this->menuLevelConfig['stdWrap.']['cObject.']['100.'] = [
-            'userFunc' => MenuProcessor::class . '->getFinalLinkParameter',
-            'stdWrap.' => [
-                'wrap' => ', "finalLinkParameter":|'
-            ]
-        ];
-        parent::buildConfiguration();
-    }
-
-    public function getFinalLinkParameter()
-    {
-        $page = $this->cObj->data;
-        if ($page['doktype'] == PageRepository::DOKTYPE_SHORTCUT) {
-            $this->determineOriginalShortcutPage($page);
-            $shortcutPage = null;
-            try {
-                $shortcutPage = $this->getTypoScriptFrontendController()->sys_page->getPageShortcut(
-                    $page['shortcut'],
-                    $page['shortcut_mode'],
-                    $page['uid'],
-                    20,
-                    [],
-                    true
-                );
-            } catch (\Exception $e) {
-            }
-            $finalLinkParameter = $shortcutPage['uid'];
-        } elseif ($page['doktype'] == PageRepository::DOKTYPE_LINK) {
-            $url = $this->pageRepository->getExtURL($page);
-            $finalLinkParameter = $url;
-        } else {
-            $finalLinkParameter = $page['uid'];
-        }
-
-        return $this->jsonEncode($finalLinkParameter);
     }
 
     public function prepareLevelLanguageConfiguration()
